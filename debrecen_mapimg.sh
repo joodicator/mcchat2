6c437fc02d4a3ee503f2aac38def29bca7d3be3d
joo <john.macinroe@gmail.com>
2019-07-03 21:02:11 +0200

    Delete debrecen_mapimg.sh

--- debrecen_mapimg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec python3 $(dirname $0)/mcchat2.py \
-    debrecen.fto.zone:32222 \
-    hafydd@live.com \
-    --password-file /home/jc/git/PageBot/state/mcchat-debrecen.pass \
-    --plugins mapimg \
-    "$@"
